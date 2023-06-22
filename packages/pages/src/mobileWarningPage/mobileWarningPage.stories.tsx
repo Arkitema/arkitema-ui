a0ad147c5023cf6b8817b1834fc081a7bf2ed108
ingf <ingf@itu.dk>
2023-06-22 14:15:51 +0200

    loginpage and mobile warning stories

---
+++ packages/pages/src/mobileWarningPage/mobileWarningPage.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import type { Meta, StoryObj } from '@storybook/react'
+import { MobileWarningPage } from '.'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+const meta: Meta<typeof MobileWarningPage> = {
+  title: 'MobileWarningPage',
+  component: MobileWarningPage,
+}
+
+export default meta
+
+type Story = StoryObj<typeof MobileWarningPage>
+
+export const Primary: Story = {
+  args: {
+      link: '',
+      title: 'Arkitema UI'
+  },
+  decorators: [
+    (StoryComponent) => (
+      <Router>
+        <StoryComponent />
+      </Router>
+    ),
+  ],
+}
