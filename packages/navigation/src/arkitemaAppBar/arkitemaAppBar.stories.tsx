b669ed36b41156f58317bbda1a4215480b7b0fa9
ingf <ingf@itu.dk>
2023-06-22 11:00:37 +0200

    initiated storybook; created appBar story

---
+++ packages/navigation/src/arkitemaAppBar/arkitemaAppBar.stories.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import type { Meta, StoryObj } from '@storybook/react'
+import { ArkitemaAppBar, LinkItem } from '.'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Typography } from '@mui/material'
+import { LogoProps, Logo, theme } from '@arkitema/brand'
+
+const meta: Meta<typeof ArkitemaAppBar> = {
+  title: 'ArkitemaAppBar',
+  component: ArkitemaAppBar,
+}
+
+export default meta
+
+type Story = StoryObj<typeof ArkitemaAppBar>
+
+const navItems: LinkItem[] = [
+    { link: "/projects", title: "Projects" },
+    { link: "/constructions", title: "Constructions" },
+    { link: "/materials", title: "Materials" },
+  ];
+
+export const Primary: Story = {
+  args: {
+    showPageLinks: true,
+    navItems: navItems,
+    logoProps: {link: '', title: 'Arkitema UI'}
+  },
+  decorators: [
+    (StoryComponent) => (
+      <Router>
+        <StoryComponent />
+      </Router>
+    ),
+  ],
+}
+
+
