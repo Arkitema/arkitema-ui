71a39512235393faf01ae1c35158e70470a5f912
ingf <ingf@itu.dk>
2023-06-22 11:49:26 +0200

    fixed logo story; added intro and fixed readme

--- packages/brand/src/logo/logo.stories.tsx
+++ packages/brand/src/logo/logo.stories.tsx
@@ -1,6 +1,7 @@
 // -- Required routing using -save-dev storybook-addon-react-router-v6 but is not compatible with storybook 7
 import type { Meta, StoryObj } from '@storybook/react'
 import { Logo } from '.'
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const meta: Meta<typeof Logo> = {
   title: 'Logo',
@@ -14,8 +15,14 @@ type Story = StoryObj<typeof Logo>
 export const Primary: Story = {
   args: {
     link: '/not_working_currently',
-    sxImage: { height: '32px', width: '200px' },
     sxText: {},
     title: 'Carbon',
   },
+  decorators: [
+    (StoryComponent) => (
+      <Router>
+        <StoryComponent />
+      </Router>
+    ),
+  ],
 }
