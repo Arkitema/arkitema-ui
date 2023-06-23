d691de4574aef2411a67768f2efacaa2b7d4bfbc
ingf <ingf@itu.dk>
2023-06-23 10:24:14 +0200

    added loading story

---
+++ packages/datafetching/src/loading/loading.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import type { Meta, StoryObj } from '@storybook/react'
+import { Loading } from '.'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+const meta: Meta<typeof Loading> = {
+  title: 'Loading',
+  component: Loading,
+}
+
+export default meta
+
+type Story = StoryObj<typeof Loading>
+
+export const Primary: Story = {
+  args: {
+    loading: true
+  },
+  decorators: [
+    (StoryComponent) => (
+      <Router>
+        <StoryComponent />
+      </Router>
+    ),
+  ],
+}
