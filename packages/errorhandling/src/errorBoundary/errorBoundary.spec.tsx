1a0629b7bfd2ab766babf97aebbe515ad2ba920b
ingf <ingf@itu.dk>
2023-06-30 11:20:20 +0200

    silenced some errors, some still remaining

--- packages/errorhandling/src/errorBoundary/errorBoundary.spec.tsx
+++ packages/errorhandling/src/errorBoundary/errorBoundary.spec.tsx
@@ -3,20 +3,15 @@ import { ErrorBoundary } from './index'
 import '@testing-library/jest-dom'
 import React from 'react'
 
-const ErrorChild = () => {
-  throw new Error('Sorry.. an error occurred')
-}
-
-const Child = () => {
-  return <h1>No error</h1>
-}
-
 describe('Error Boundary', () => {
   it('should render an error message when an error occurs', () => {
     // Temporarily mock console.error to silence
     const consoleError = console.error
     console.error = () => {}
-
+    const ErrorChild = () => {
+      throw new Error('Sorry.. an error occurred')
+    }
+    
     const { baseElement } = render(
       <ErrorBoundary>
         <ErrorChild />
@@ -31,6 +26,10 @@ describe('Error Boundary', () => {
   })
 
   it('should render the child component without error', () => {
+    const Child = () => {
+      return <h1>No error</h1>
+    }
+    
     const { baseElement } = render(
       <ErrorBoundary>
         <Child />
