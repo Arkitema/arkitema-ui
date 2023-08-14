28c50dcdb227c7a423f9c4783546d3fc6f2211d2
Christian Kongsgaard <chrk@arkitema.com>
2023-08-14 16:16:41 +0200

    chore: updated eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,40 +2,41 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    jest: true,
+    jest: true
   },
-  ignorePatterns: ['src/dataAccess/*', 'vitest.*.ts'],
+  ignorePatterns: ["src/dataAccess/*", "vitest.*.ts"],
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'plugin:import/typescript',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:import/typescript"
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module"
   },
-  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  plugins: ["react", "react-hooks", "@typescript-eslint", "prettier"],
   rules: {
-    'react/react-in-jsx-scope': 'off',
-    camelcase: 'error',
-    'spaced-comment': ['error', 'always', { markers: ['/'] }],
-    quotes: ['error', 'single'],
-    'no-duplicate-imports': 'error',
-    'prettier/prettier': 'error',
-    "no-unused-vars": ["error", { "args": "none"}]
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    camelcase: "error",
+    "spaced-comment": ["error", "always", { markers: ["/"] }],
+    quotes: ["error", "single"],
+    "no-duplicate-imports": "error",
+    "prettier/prettier": "error",
+    "no-unused-vars": ["error", { "args": "none" }]
   },
   settings: {
     react: {
-      version: 'detect',
+      version: "detect"
     },
-    'import/resolver': {
-      typescript: {},
-    },
-  },
-}
+    "import/resolver": {
+      typescript: {}
+    }
+  }
+};
